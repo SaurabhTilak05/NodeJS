abdeb6f148b068392a490a1643ed22d22f03db79
saurabh <saurbhtilak6@gmail.com>
2025-06-07 15:08:53 +0530

    Registration Project

---
+++ RegistrationProject/index.js
@@ -0,0 +1,47 @@
+let express=require("express");
+let path=require("path");
+let bodyParser=require("body-parser");
+let mysql=require("mysql2");
+const { emit } = require("process");
+
+let app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+let p=path.join(__dirname+"/public");
+app.get("/",(req,res)=>{
+    res.sendFile(p+"/register.html");
+});
+
+let con=mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Saurabh@2003",
+    database:"registration"
+});
+
+con.connect((err)=>{
+    if(err)console.log("Connection failed !!!")
+    else console.log("Connection successfull !!");
+})
+
+app.post("/save",(req,res)=>{
+    let {name,email,phn,qualification,dob,university}=req.body;
+    con.query("insert into Registration values(?,?,?,?,?,?)",[name,email,phn,qualification,dob,university],(err,result)=>{
+        if(err){ 
+            console.log("Not inserted"+err);
+		}else{
+		   if(result.affectedRows>0){
+                res.send("Registartion successfull");
+			}
+		   else{
+			   res.send("Registartion failed");
+		   }
+		}
+    })
+});
+
+app.listen(8080,()=>{
+    console.log("server started.....");
+});
+
