5a383684e5874ddc20cd73697ca8fe1568a7e5ac
saurabh <saurbhtilak6@gmail.com>
2025-06-08 15:52:33 +0530

    EJS Project Developing run time table

---
+++ EJSTableProject/index.js
@@ -0,0 +1,19 @@
+const bodyParser=require("body-parser");
+let express=require("express");
+let app=express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/table",(req,res)=>{
+    res.render("table.ejs",{no:0});
+});
+app.post("/table",(req,res)=>{
+    let {num}=req.body;
+    res.render("table.ejs",{no:num});
+});
+app.listen(3000,()=>{
+    console.log("Server Start");
+})
+
+
