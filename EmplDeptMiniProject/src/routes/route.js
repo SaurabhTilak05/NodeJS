8fe780f14fafce0f3bbad9e48055edb5232e82ef
saurabh <saurbhtilak6@gmail.com>
2025-06-14 12:55:02 +0530

    this is router file

--- EmplDeptMiniProject/src/routes/route.js
+++ EmplDeptMiniProject/src/routes/route.js
@@ -5,4 +5,9 @@ let router=express.Router();
 router.post("/adddept",deptctrl.saveDept );
 router.get("/",deptctrl.homePage);
 router.get("/newdept",deptctrl.newDept);
+router.get("/viewalldept",deptctrl.getAllDept)
+router.get("/deldept", deptctrl.delDept);
+router.get("/upddept",deptctrl.updateDept);
+router.post("/updatedept",deptctrl.deptFinalUpdate);
+router.get("/searchDeptByName",deptctrl.searchDeptByUsingName);
 module.exports=router;
