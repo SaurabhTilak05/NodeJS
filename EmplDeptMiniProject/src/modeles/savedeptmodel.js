d67e00d0a0f2614188beb01edbc08127249c4236
saurabh <saurbhtilak6@gmail.com>
2025-06-14 12:54:10 +0530

    This file is save dept models

--- EmplDeptMiniProject/src/modeles/savedeptmodel.js
+++ EmplDeptMiniProject/src/modeles/savedeptmodel.js
@@ -11,3 +11,61 @@ exports.saveDept=(deptname)=>{
        })
     });
 }
+
+exports.getAllDept=()=>{
+    return new Promise((resolve,reject)=>{
+        db.query("select *from dept",(err,result)=>{
+            if(err)
+            {
+                reject(err);
+            }
+            else{
+                resolve(result);
+            }
+
+        });
+    });
+}
+
+exports.delDeptById=(did)=>{
+    return new Promise((resolve, reject)=>{
+        db.query("delete from dept where deptid=?",[did],(err,result)=>{
+            if(err)
+            {
+                reject(err);
+            }
+            else{
+                resolve("success");
+            }
+        });
+    });
+}
+
+exports.finalUpdateDept=(did,name)=>{
+    return new Promise((resolve,reject)=>{
+         db.query("update dept set deptname=? where deptid=?",[name,did],(err,result)=>{
+         if(err){
+            reject(err);
+         }
+         else{
+            resolve("success");
+         }
+           
+         });
+    });
+   
+}
+
+exports.getDeptByName=(deptname)=>{
+    return new Promise((resolve,reject)=>{
+        db.query("select *from dept where deptname like '%"+deptname+"%'",(err,result)=>{
+            if(err)
+            {
+                reject(err);
+            }
+            else{
+                resolve(result);
+            }
+        })
+    });
+}
