bf19c8dc83e5752672a626dce205152c22213266
saurabh <saurbhtilak6@gmail.com>
2025-06-14 12:53:18 +0530

    This is dept controller file

--- EmplDeptMiniProject/src/controllers/deptcontroller.js
+++ EmplDeptMiniProject/src/controllers/deptcontroller.js
@@ -16,3 +16,58 @@ exports.homePage=(req,res)=>{
 exports.newDept=(req,res)=>{
     res.render("adddept.ejs", {msg:""});
 }
+exports.getAllDept=(req,res)=>{
+    let promise=deptmodel.getAllDept();
+    promise.then((result)=>{
+        res.render("viewdept.ejs",{deptList:result});
+    });
+    promise.catch((err)=>{
+        res.send(err);
+    });
+}
+exports.delDept=(req, res)=>{
+   let did=parseInt(req.query.did);
+   let promise=deptmodel.delDeptById(did);
+   promise.then((result)=>{
+         let p=deptmodel.getAllDept();
+    p.then((result)=>{
+        res.render("viewdept.ejs",{deptList:result});
+    });
+    p.catch((err)=>{
+        res.send(err);
+    });
+   });
+   promise.catch((err)=>{
+
+   });
+}
+exports.updateDept=(req,res)=>{
+    res.render("updatedept.ejs",{deptName:req.query.dn,
+                                    deptId:req.query.did
+    });
+}
+
+exports.deptFinalUpdate=(req,res)=>{
+    let {id,name}=req.body;
+    let promise=deptmodel.finalUpdateDept(id,name);
+    promise.then((result)=>{
+
+     let p=deptmodel.getAllDept();
+    p.then((result)=>{
+        res.render("viewdept.ejs",{deptList:result});
+    });
+    });
+    promise.catch((err)=>{
+        res.send("Dept Not Updated... ");
+    });
+}
+
+exports.searchDeptByUsingName=((req,res)=>{
+    let name=req.query.dn;
+   let promise=deptmodel.getDeptByName(name);
+   promise.then((result)=>{
+    res.json(result);
+   }).catch((err)=>{
+    res.send("Something went wrong..");
+   })
+});
