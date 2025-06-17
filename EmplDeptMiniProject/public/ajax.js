20525dba75cec0312c9b260ddd1ff351d880f8a1
saurabh <saurbhtilak6@gmail.com>
2025-06-17 12:23:56 +0530

    Ajax file

--- EmplDeptMiniProject/public/ajax.js
+++ EmplDeptMiniProject/public/ajax.js
@@ -28,3 +28,58 @@ let searchDept=(str)=>{
     xhttp.open("get","/searchDeptByName?dn="+str,true);
     xhttp.send();
 }
+
+
+let checkEmailExistance=(str)=>{
+
+    let xhttp=new XMLHttpRequest();
+    xhttp.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200){
+           if(this.responseText.length>0)
+           {
+            document.getElementById("lableMsg").innerHTML=this.responseText;
+            document.getElementById("uemail").focus();
+           }
+           else{
+             document.getElementById("lableMsg").innerHTML="";
+           }
+        }
+    };
+    xhttp.open("get","/searchEmail?e="+str,true);
+    xhttp.send();
+}
+
+let getEmployeeByDept=()=>{
+    let deptId=parseInt(document.getElementById("did").value);
+    let xhttp=new XMLHttpRequest();
+    xhttp.onreadystatechange=function(){
+
+        if(this.readyState==4 && this.status==200){
+            let jsonArr=JSON.parse(this.responseText);
+            document.getElementById("tblBody").innerHTML="";
+            let tableBody=document.getElementById("tblBody");
+            let str="";
+
+             if (jsonArr.length == 0) {
+                str = "<tr><td colspan='5'>No Employees Found for this Department.</td></tr>";
+                tableBody.innerHTML=str;
+            } else {
+
+
+            for(let i=0;i<jsonArr.length;i++)
+            {
+                str=str+"<tr>";
+                str=str+"<td>"+jsonArr[i].name+"</td>";
+                str=str+"<td>"+jsonArr[i].email+"</td>";
+                str+="<td>"+jsonArr[i].contact+"</td>";
+               str += "<td><img src='/images/" + jsonArr[i].photo + "' alt='Employee Photo' width='50' height='50'></td>";  
+               str += "<td>" + jsonArr[i].deptname + "</td>"; 
+              
+            }
+            tableBody.innerHTML=str;
+        }
+    }
+    };
+    xhttp.open("get","/getEmpByDeptId?deptId="+deptId,true);
+    xhttp.send();
+}
