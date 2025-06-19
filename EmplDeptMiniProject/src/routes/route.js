6d41f865106fdd87e89393b175ae920255b27d3a
saurabh <saurbhtilak6@gmail.com>
2025-06-19 23:46:45 +0530

    this is routes file it contain all api

--- EmplDeptMiniProject/src/routes/route.js
+++ EmplDeptMiniProject/src/routes/route.js
@@ -1,7 +1,10 @@
 let express=require("express");
 let deptctrl=require("../controllers/deptcontroller.js");
 let router=express.Router();
+let empctrl=require("../controllers/empctrl.js");
+let upload=require("../middleware/fileupload.js");
 
+//Dept
 router.post("/adddept",deptctrl.saveDept );
 router.get("/",deptctrl.homePage);
 router.get("/newdept",deptctrl.newDept);
@@ -10,4 +13,20 @@ router.get("/deldept", deptctrl.delDept);
 router.get("/upddept",deptctrl.updateDept);
 router.post("/updatedept",deptctrl.deptFinalUpdate);
 router.get("/searchDeptByName",deptctrl.searchDeptByUsingName);
+
+
+//Employee
+router.get("/newemployee",empctrl.newemp);
+router.get("/getEmpByDeptId",empctrl.getEmployeeByDeptId);
+router.get("/viewallemp",empctrl.getAllEmp);
+router.get("/deleteEmp",empctrl.deleteEmp);
+router.get("/searchEmpByDept",empctrl.searchEmp);
+router.get("/viewEmp",empctrl.viewEmp);
+router.get("/updEmp",empctrl.updateEmp);
+
+router.post("/updateemp",empctrl.empFinalUpdate);
+
+router.post("/saveemp",upload.single("photo"),empctrl.saveEmployee);
+router.get("/searchEmail",empctrl.verifyEmail);
+
 module.exports=router;
