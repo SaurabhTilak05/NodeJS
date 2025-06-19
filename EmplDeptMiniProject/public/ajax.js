8577a55a0930ffb5df8f8fd4f44873fe47b28fae
saurabh <saurbhtilak6@gmail.com>
2025-06-19 23:26:38 +0530

    Ajax file it is use to partial page updation

--- EmplDeptMiniProject/public/ajax.js
+++ EmplDeptMiniProject/public/ajax.js
@@ -49,37 +49,79 @@ let checkEmailExistance=(str)=>{
     xhttp.send();
 }
 
-let getEmployeeByDept=()=>{
-    let deptId=parseInt(document.getElementById("did").value);
+// let getEmployeeByDept=()=>{
+//     let deptId=parseInt(document.getElementById("did").value);
+//     let xhttp=new XMLHttpRequest();
+//     xhttp.onreadystatechange=function(){
+
+//         if(this.readyState==4 && this.status==200){
+//             let jsonArr=JSON.parse(this.responseText);
+//             document.getElementById("tblBody").innerHTML="";
+//             let tableBody=document.getElementById("tblBody");
+//             let str="";
+
+//              if (jsonArr.length == 0) {
+//                 str = "<tr><td colspan='5'>No Employees Found for this Department.</td></tr>";
+//                 tableBody.innerHTML=str;
+//             } else {
+
+
+//             for(let i=0;i<jsonArr.length;i++)
+//             {
+//                 str=str+"<tr>";
+//                 str=str+"<td>"+jsonArr[i].name+"</td>";
+//                 str=str+"<td>"+jsonArr[i].email+"</td>";
+//                 str+="<td>"+jsonArr[i].contact+"</td>";
+//                str += "<td><img src='/images/" + jsonArr[i].photo + "' alt='Employee Photo' width='50' height='50'></td>";  
+//                str += "<td>" + jsonArr[i].deptname + "</td>"; 
+              
+//             }
+//             tableBody.innerHTML=str;
+//         }
+//     }
+//     };
+//     xhttp.open("get","/getEmpByDeptId?deptId="+deptId,true);
+//     xhttp.send();
+// }
+
+
+
+
+let searchEmpByDept=(str)=>{
     let xhttp=new XMLHttpRequest();
     xhttp.onreadystatechange=function(){
-
         if(this.readyState==4 && this.status==200){
-            let jsonArr=JSON.parse(this.responseText);
-            document.getElementById("tblBody").innerHTML="";
-            let tableBody=document.getElementById("tblBody");
-            let str="";
+           let jsonOjb=JSON.parse(this.responseText);
+           let tablebody=document.getElementById("tblBody");
+            tablebody.innerHTML="";
+           jsonOjb.forEach((element ,index)=> {
+                let row=document.createElement("tr");
+                let col=document.createElement("td");
+                col.innerHTML=(index+1);
+                row.appendChild(col);
+                col=document.createElement("td");
+                col.innerHTML=element.name;
+                row.append(col);
+                col=document.createElement("td");
+                col.innerHTML=element.deptname;
+                row.append(col);
+                col=document.createElement("td");
+                col.innerHTML=element.contact;
+                row.append(col);
+                col=document.createElement("td");
+                col.innerHTML="<a href='/deleteEmp?eid="+element.eid+"' >DELETE</a>";
+                row.appendChild(col);
+                col=document.createElement("td");
+                col.innerHTML="<a href='/updEmp?name="+element.name+"&id="+element.eid+"' >UPDATE</a>";
+                row.appendChild(col);
+                col=document.createElement("td");
+                col.innerHTML="<a href='/viewEmp?eid="+element.eid+"' >View Details</a>";
+                row.appendChild(col);
 
-             if (jsonArr.length == 0) {
-                str = "<tr><td colspan='5'>No Employees Found for this Department.</td></tr>";
-                tableBody.innerHTML=str;
-            } else {
-
-
-            for(let i=0;i<jsonArr.length;i++)
-            {
-                str=str+"<tr>";
-                str=str+"<td>"+jsonArr[i].name+"</td>";
-                str=str+"<td>"+jsonArr[i].email+"</td>";
-                str+="<td>"+jsonArr[i].contact+"</td>";
-               str += "<td><img src='/images/" + jsonArr[i].photo + "' alt='Employee Photo' width='50' height='50'></td>";  
-               str += "<td>" + jsonArr[i].deptname + "</td>"; 
-              
-            }
-            tableBody.innerHTML=str;
+                tablebody.appendChild(row);
+           });
         }
-    }
     };
-    xhttp.open("get","/getEmpByDeptId?deptId="+deptId,true);
+    xhttp.open("get","/searchEmpByDept?dept="+str,true);
     xhttp.send();
-}
+};
