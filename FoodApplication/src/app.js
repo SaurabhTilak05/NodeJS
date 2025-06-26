2553a7b43b3e933f6d06b4a5a6bd6844cd032eda
saurabh <saurbhtilak6@gmail.com>
2025-06-26 12:15:22 +0530

    This is use for import all dependencies

--- FoodApplication/src/app.js
+++ FoodApplication/src/app.js
@@ -5,10 +5,12 @@ let db=require("../db.js");
 let router=require("./routes/route.js");
 require("dotenv").config();
 
-let app=express();
+
+let app=express(); // using app express
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("views engine", "ejs");
 app.use("/", router);
+
 
 module.exports=app;
